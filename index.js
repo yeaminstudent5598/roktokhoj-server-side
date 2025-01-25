546b301134fcc1618316925c40e6e022b6fff575
yeaminstudent5598 <yeaminstudent5598@gmail.com>
2025-01-25 18:35:30 +0600

    Refactor database schema for user roles

--- index.js
+++ index.js
@@ -536,7 +536,7 @@ run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Roktokhoj server is running!');
-});   
+});     
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
